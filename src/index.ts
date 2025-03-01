c5c2a8bb827ec42d62b246f91b7f2649f39a4132
Soham Das <publicsoham.24@gmail.com>
2025-03-01 20:23:55 +0530

    fix: allowed cors for all origins

--- src/index.ts
+++ src/index.ts
@@ -8,24 +8,25 @@ import cors from "cors";
 config();
 const port = process.env.PORT || 3000;
 
-const allowedOrigins = process.env.ALLOWED_ORIGINS
-  ? process.env.ALLOWED_ORIGINS.split(",")
-  : [];
+// const allowedOrigins = process.env.ALLOWED_ORIGINS
+//   ? process.env.ALLOWED_ORIGINS.split(",")
+//   : [];
 
 const app = express();
 app.use(express.json({ limit: "100mb" }));
-app.use(
-  cors({
-    origin(origin, callback) {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-  })
-);
+// app.use(
+//   cors({
+//     origin(origin, callback) {
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) !== -1) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//   })
+// );
+app.use(cors());
 
 // controllers
 app.use("/api", UploadDataRouter);
